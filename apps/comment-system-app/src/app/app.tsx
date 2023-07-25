62532a1cad472a02c78645debf7d39730d1036cf
Asep Ardi <ardi.winardi91@gmail.com>
2023-07-25 08:46:32 +0700

    init project

---
+++ apps/comment-system-app/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="comment-systems" />
+    </div>
+  );
+}
+
+export default App;
